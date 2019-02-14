448a9ecfd27d8c886e3cce5b9ad3c14d246dfb30
Zarafshan Khalid <zkhalid@uh.edu>
2019-02-14 14:46:35 -0600

    Task 2 complete

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -69,6 +69,7 @@ class JobsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def job_params
-      params.require(:job).permit(:name)
+      params.require(:job).permit(:name, :location)
+
     end
 end
